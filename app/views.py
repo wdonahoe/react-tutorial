b59ef651cd8d27dd3cdd0db4998e1cdec4efeee5
wdonahoe <wdonahoe@udel.edu>
2015-07-10 11:48:50 -0400

    added react and jquery functionality

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ view = Blueprint('view',__name__,template_folder='templates')
 
 @view.route('/jquery/',methods=['GET'])
 def jquery():
-	return render_template('jquery/index.html',title='Jquery')
+	return render_template('jquery/index.html',title='JQuery')
 
 @view.route('/react/',methods=['GET'])
 def react():
